65e89354ccb0725e7172792093b8ff10fb8235a1
KriksalisProgrammer <67312034+KriksalisProgrammer@users.noreply.github.com>
2022-05-02 22:22:11 +0300

    Rotation Tower

--- Assets/Script/Turret.cs
+++ Assets/Script/Turret.cs
@@ -4,9 +4,59 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    public  Transform target;
+    private  Transform _target;
+    private string _enemyTag = "Enemy";
+
+    public Transform rotateTurret;
 
     public float range=15f;
+    public float turnSpeed = 10f;
+
+    
+
+    private void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    private void Update()
+    {
+        if(_target==null)
+            return;
+
+        Vector3 direction = _target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        Vector3 rotation = Quaternion.Lerp(rotateTurret.rotation,lookRotation,Time.deltaTime*turnSpeed).eulerAngles;
+        rotateTurret.rotation = Quaternion.Euler(-90,rotation.y, 0);
+    }
+
+    private void UpdateTarget()
+    {
+        GameObject[] enemys = GameObject.FindGameObjectsWithTag(_enemyTag);
+
+        float shortDistance = Mathf.Infinity;
+
+        GameObject nearestEnemy = null;
+
+        foreach(var enemy in enemys)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position,enemy.transform.position);
+            if(distanceToEnemy < shortDistance)
+            {
+                shortDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortDistance<=range )
+        {
+            _target = nearestEnemy.transform;
+        }
+        else
+        {
+            _target = null;
+        }
+    }
 
     private void OnDrawGizmosSelected()
     {
