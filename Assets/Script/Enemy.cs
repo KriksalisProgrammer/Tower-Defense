ce8ffc269f342448f38b35963c1234f12ea4a3b6
KriksalisProgrammer <67312034+KriksalisProgrammer@users.noreply.github.com>
2022-05-02 20:30:38 +0300

    Move Enemy

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform _target;
+    private int _wavePointIndex = 0;
+    private void Start()
+    {
+        _target = Waypoints.waypoints[0];
+    }
+    private void Update()
+    {
+        Vector3 direction = _target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, _target.position) < 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (_wavePointIndex >= Waypoints.waypoints.Length-1)
+        {
+            Destroy(gameObject);
+        }
+        _wavePointIndex++;
+        _target = Waypoints.waypoints[_wavePointIndex];
+    }
+}
